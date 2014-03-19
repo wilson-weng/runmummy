99d199195e8d0a6046bf79e3f294e5165c523d71
wilson-weng <swenghkust@gmail.com>
2014-03-19 22:28:16 +0800

    initialze

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Runmummy::Application.config.secret_key_base = '9b331b9d30d3e5a6e4c5fdb3181f28cec0df4975c903c2403cdffdfaeeab27a6508c44bfc71d1d4751e40d41d197bce83e315985875427a591138b1b638699fe'
